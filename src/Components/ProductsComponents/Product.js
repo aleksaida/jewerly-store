006e3a41e894372f956c2b6d1e7137d36cb74e4b
aleksaida <aida1384@mail.ru>
2022-12-10 15:47:17 +0300

    css

--- src/Components/ProductsComponents/Product.js
+++ src/Components/ProductsComponents/Product.js
@@ -12,7 +12,7 @@ const Product = ({product}) => {
     <div className="cardContainer">
     <div className="prodCard">
         <img className="productImage" src={`./${product.img}.jfif`} alt="prod"/>
-        <h2>{product.name}</h2>
+        <h2 className="nameProd">{product.name}</h2>
         <h2>{product.price} Р.</h2>
         <ChangeQuantity quantity={quantity} setQuantity={setQuantity}/>
         <button className="cartBtn" onClick={() => {dispatch(addItemToCart({product, quantity}));
